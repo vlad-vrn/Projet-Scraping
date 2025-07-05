b077ef8bf1565db6e65c1cc9e104d15decc85886
vlad-vrn <varenne.vw@gmail.com>
2025-07-05 22:24:58 +0200

    Z

---
+++ main.py
@@ -0,0 +1,14 @@
+﻿import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+
+url = "https://books.toscrape.com"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+images = soup.find_all('article', class_ = "product_pod")
+aside = soup.find('aside')
+for child in aside.children:
+    print(child.text)
