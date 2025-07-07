2b067a6ced64a4b2bd8630c42a0cde34815d8ba4
vlad-vrn <varenne.vw@gmail.com>
2025-07-07 22:25:16 +0200

    Z

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-images = soup.find_all('article', class_ = "product_pod")
-aside = soup.find('aside')
-for child in aside.children:
-    print(child.text)
+sidecat = soup.find('div', class_='side_categories')
+categories_div = sidecat.find('ul').find('li').find('ul')
+categories =[child.text.strip() for child in categories_div.children if child.name]
+
+images = soup.find('section').find_all('img')
+for image in images:
+    print(image['src'])
+
+#images1 = [child.text.strip() for child in images.children['src']]
+#pprint(images1)
