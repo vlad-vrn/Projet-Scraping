e2890bb145ab0a2579604f07e712dcca9c2e7635
vlad-vrn <varenne.vw@gmail.com>
2025-07-08 01:28:28 +0200

    mrlefaire!!!!!!!

--- exercice1.py
+++ exercice1.py
@@ -11,16 +11,14 @@ def find_number(url):
     responselink = requests.get(url)
     sub_soup = BeautifulSoup(responselink.text, 'html.parser')
     former = sub_soup.find('form', class_="form-horizontal")
-    return former.find('strong').text.strip()
+    return int(former.find('strong').text.strip())
 
 def main(n):
     aside_div = soup.find('div', class_="side_categories")
     categories = aside_div.find('ul').find('li').find('ul')
-    for category in categories.children:
-        #numb = find_number(category.get('href'))
-        link = category.find('a')
-        print(category.get('href'))
-        #numb = find_number(link.get('href'))
-        #if numb<n:
-        #    print(category.text.strip())
-main(5)
+    links = categories.find_all('a')
+    for link in links:
+        numb = find_number(initialUrl + link['href'])
+        if numb<n:
+            print(link.text.strip())
+main(2)
