19d9e5a194ca676407fdc9d6f731175d58741337
vlad-vrn <varenne.vw@gmail.com>
2025-07-08 11:30:47 +0200

    err

--- fdf.py
+++ fdf.py
@@ -7,6 +7,26 @@ initialUrl = "https://fondationdefrance.org/fr/tag-vulnerabilites"
 soup = BeautifulSoup(requests.get(initialUrl).text, "html.parser")
 
 columns = soup.find('div', id="template-amJs7jvN#2", class_="uk-margin")
-text = columns.text.strip() if columns.name else ""
 
-pprint(text)
+
+def func1():
+    text = columns.text.strip() if columns.name else ""
+    pprint(text)
+
+
+
+#for column in columns.children:
+#    title = column.find('h3').find('a')
+#    if title.name:
+#        pprint(title.text.strip())
+
+#title = columns.find('h3').find('a')
+#pprint(title.text.strip())
+
+def title_extraction():
+    for a in columns.find_all('h3'):
+        title = a.find('a')
+        if title.name:
+            pprint(title.text.strip())
+
+title_extraction()
